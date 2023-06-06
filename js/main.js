63d26cad1c777f6d546cfca85c986f1283cb1d2a
Kim Oleg <88618558+KimOleg12356789@users.noreply.github.com>
2023-06-06 15:41:23 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+$("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+});
+
+
+$("#menu2").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+});
+
+
+
+
+});
+
+
+
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.mt-mobile');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('nav-open');
+    body.classList.toggle('body-open');
+    burger.classList.toggle('burger-open');
+    
+});
